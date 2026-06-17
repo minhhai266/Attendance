5c4f2927c3be04dcbbfd7afed71714cefbec76a8
QPTA <donhatquang401@gmail.com>
2026-06-17 16:30:07 +0700

    WIP: local changes

--- src/main/java/com/attendenceSystem/user/entity/User.java
+++ src/main/java/com/attendenceSystem/user/entity/User.java
@@ -2,6 +2,12 @@ package com.attendenceSystem.user.entity;
 
 import java.time.Instant;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,15 +19,29 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "email")
     private String email;
+    @Column(name = "password")
     private String password;
+    @Column(name = "full_name")
     private String fullName;
+    @Column(name = "avatar")
     private String avatar;
+    @Column(name = "role")
     private String role;
+    @Column(name = "status")
     private String status;
+    @Column(name = "created_at")
     private Instant createdAt;
+    @Column(name = "updated_at")
     private Instant updatedAt;
+    @Column(name = "deleted")
     private Boolean deleted;
 }
