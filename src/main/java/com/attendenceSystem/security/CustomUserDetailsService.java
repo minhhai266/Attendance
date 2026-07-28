cdd5a63abbdf31f12e6081444bfb1c2635b43d3e
QPTA <donhatquang401@gmail.com>
2026-07-28 09:58:50 +0700

    Refactor sql searchh username and email in loadUserByUserName to search username

--- src/main/java/com/attendenceSystem/security/CustomUserDetailsService.java
+++ src/main/java/com/attendenceSystem/security/CustomUserDetailsService.java
@@ -20,7 +20,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
                 User user = userRepository
-                                .findByUsernameOrEmail(username, username)
+                                .findByUsername(username)
                                 .orElseThrow(() -> new UsernameNotFoundException("Không tìm thấy User"));
 
                 return CustomUserDetails.fromUser(user);
